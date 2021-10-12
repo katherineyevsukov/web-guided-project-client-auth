9f2fccb9b2c517d2ffce0978d5cadcca7d3fec9b
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-10-12 13:48:44 -0400

    lecture work-in-progress

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import { Route, Redirect} from 'react-router-dom';
 
+//Private Route
+//1. Have the same functionality as Route.
+//2. It renders the same component if we are logged in with the same props.
+//3. Redirect to login if not logged in.
+
+const PrivateRoute = () => {
+    
+}
+
+export default PrivateRoute;
